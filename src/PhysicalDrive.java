bc38019294a2abc891377d973f00c00a571870e4
BT_1E10_05 <j>
2022-03-17 12:30:22 -0400

    f

--- src/PhysicalDrive.java
+++ src/PhysicalDrive.java
@@ -1,3 +1,13 @@
-public class PhysicalDrive extends PhysicalVolume{
+import java.util.UUID;
 
+public class PhysicalDrive extends PhysicalVolume      {
+    private UUID id;
+    public PhysicalDrive(int size, String name{
+        super(size,name);
+        id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return super.getId();
+    }
 }
--- src/PhysicalVolume.java
+++ src/PhysicalVolume.java
@@ -1,2 +1,33 @@
+import java.util.UUID;
+import java.util.ArrayList;
+
 public class PhysicalVolume {
+    private int size;
+    private String name;
+    private UUID id;
+    public static ArrayList<PhysicalDrive> drive = new ArrayList<PhysicalDrive>;
+    private PhysicalDrive pDrive;
+
+    public PhysicalVolume(int size,String name){
+        this.size = size;
+        this.name = name;
+        id = UUID.randomUUID();
+        drive.add(pDrive);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public int getpDriveSize() {
+        return pDrive.getSize();
+    }
 }
